68ba2dedfdb78b91111c2f5bee2dcee04cc93707
Alan Mimms <alanmimms@gmail.com>
2019-01-04 15:58:59 +0000

    Expand and fix ADDC/DA, clearer descriptions

--- sim.test.js
+++ sim.test.js
@@ -7,7 +7,7 @@ Object.keys(sim.SFRs).forEach(name => global[name] = sim.SFRs[name]);
 
 
 //////////// RLC ////////////
-test('should run RLC A=0x80,CY=0 and produce A=00 and CY=1', () => {
+test('RLC A=0x80,CY=0 = A=00,CY=1', () => {
   cpu.pmem[0x100] = 0x33;       // RLC A
   cpu.SFR[ACC] = 0x80;
   cpu.SFR[PSW] = 0;
@@ -19,7 +19,7 @@ test('should run RLC A=0x80,CY=0 and produce A=00 and CY=1', () => {
   expect(cpu.SFR[ACC]).toBe(0x00);
 });
 
-test('should run RLC A=0x08,CY=0 and produce A=10 and CY=0', () => {
+test('RLC A=0x08,CY=0 = A=10,CY=0', () => {
   cpu.pmem[0x100] = 0x33;       // RLC A
   cpu.SFR[ACC] = 0x08;
   cpu.SFR[PSW] = 0;
@@ -31,7 +31,7 @@ test('should run RLC A=0x08,CY=0 and produce A=10 and CY=0', () => {
   expect(cpu.SFR[ACC]).toBe(0x10);
 });
 
-test('should run RLC A=0x80,CY=1 and produce A=01 and CY=1', () => {
+test('RLC A=0x80,CY=1 = A=01,CY=1', () => {
   cpu.pmem[0x100] = 0x33;       // RLC A
   cpu.SFR[ACC] = 0x80;
   cpu.SFR[PSW] = 0;
@@ -44,7 +44,7 @@ test('should run RLC A=0x80,CY=1 and produce A=01 and CY=1', () => {
   expect(cpu.SFR[ACC]).toBe(0x01);
 });
 
-test('should run RLC A=0x08,CY=1 and produce A=11 and CY=0', () => {
+test('RLC A=0x08,CY=1 = A=11,CY=0', () => {
   cpu.pmem[0x100] = 0x33;       // RLC A
   cpu.SFR[ACC] = 0x08;
   cpu.SFR[PSW] = 0;
@@ -57,7 +57,7 @@ test('should run RLC A=0x08,CY=1 and produce A=11 and CY=0', () => {
   expect(cpu.SFR[ACC]).toBe(0x11);
 });
 
-test('should walk bit through RLC as CY=1 and eventually arriving again in CY=1', () => {
+test('RLC CY=1 bit walk', () => {
   cpu.pmem[0x100] = 0x33;       // RLC A
   cpu.SFR[ACC] = 0x00;
   cpu.SFR[PSW] = 0;
@@ -80,7 +80,7 @@ test('should walk bit through RLC as CY=1 and eventually arriving again in CY=1'
 
 
 //////////// RRC ////////////
-test('should run RRC A=0x01,CY=0 and produce A=00 and CY=1', () => {
+test('RRC A=0x01,CY=0 = A=00,CY=1', () => {
   cpu.pmem[0x100] = 0x13;       // RRC A
   cpu.SFR[ACC] = 0x01;
   cpu.SFR[PSW] = 0;
@@ -92,7 +92,7 @@ test('should run RRC A=0x01,CY=0 and produce A=00 and CY=1', () => {
   expect(cpu.SFR[ACC]).toBe(0x00);
 });
 
-test('should run RRC A=0x08,CY=0 and produce A=04 and CY=0', () => {
+test('RRC A=0x08,CY=0 = A=04,CY=0', () => {
   cpu.pmem[0x100] = 0x13;       // RRC A
   cpu.SFR[ACC] = 0x08;
   cpu.SFR[PSW] = 0;
@@ -104,7 +104,7 @@ test('should run RRC A=0x08,CY=0 and produce A=04 and CY=0', () => {
   expect(cpu.SFR[ACC]).toBe(0x04);
 });
 
-test('should run RRC A=0x80,CY=1 and produce A=C0 and CY=0', () => {
+test('RRC A=0x80,CY=1 = A=C0,CY=0', () => {
   cpu.pmem[0x100] = 0x13;       // RRC A
   cpu.SFR[ACC] = 0x80;
   cpu.SFR[PSW] = 0;
@@ -117,7 +117,7 @@ test('should run RRC A=0x80,CY=1 and produce A=C0 and CY=0', () => {
   expect(cpu.SFR[ACC]).toBe(0xC0);
 });
 
-test('should run RRC A=0x08,CY=1 and produce A=84 and CY=0', () => {
+test('RRC A=0x08,CY=1 = A=84,CY=0', () => {
   cpu.pmem[0x100] = 0x13;       // RRC A
   cpu.SFR[ACC] = 0x08;
   cpu.SFR[PSW] = 0;
@@ -130,7 +130,7 @@ test('should run RRC A=0x08,CY=1 and produce A=84 and CY=0', () => {
   expect(cpu.SFR[ACC]).toBe(0x84);
 });
 
-test('should walk bit through RRC as CY=1 and eventually arriving again in CY=1', () => {
+test('RRC CY=1 bit walk', () => {
   cpu.pmem[0x100] = 0x13;       // RRC A
   cpu.SFR[ACC] = 0x00;
   cpu.SFR[PSW] = 0;
@@ -153,34 +153,34 @@ test('should walk bit through RRC as CY=1 and eventually arriving again in CY=1'
 
 
 //////////////// DA ////////////////
-test(`should properly ADDC/DA CY=0,AC=0`, () => {
+function testADDC_DA(x, y) {
   const dir = 0x42;
   cpu.pmem[0x100] = 0x35;       // ADDC A,dir
   cpu.pmem[0x101] = dir;
   cpu.pmem[0x102] = 0xD4;       // DA A
 
-  for (let x = 0; x < 0x100; x += 13) {
-    cpu.iram[dir] = x;
+  cpu.iram[dir] = x;
 
-    for (let y = 0; y < 100; y += 17) {
-      const sum = x + y;
-      const digits = [sum % 10,
-                      Math.floor(sum / 10) % 10,
-                      Math.floor(sum / 100)];
-      cpu.SFR[PSW] = 0;
-      cpu.SFR[ACC] = y;
+  const addcSum = x + y;
+  const bcdsum = +('0x' + (+x.toString(16) + +y.toString(16)));
+  cpu.SFR[PSW] = 0;
+  cpu.SFR[ACC] = y;
 
-      cpu.run1(0x100);          // ADDC
-      expect(cpu.pc).toBe(0x102);
-      expect(cpu.SFR[ACC]).toBe(sum & 0xFF);
-      expect(cpu.getCY()).toBe(+(sum > 0xFF));
+  cpu.run1(0x100);          // ADDC
+  expect(cpu.pc).toBe(0x102);
+  expect(cpu.SFR[ACC]).toBe(addcSum & 0xFF);
+  expect(cpu.getCY()).toBe(+(addcSum > 0xFF));
 
-      cpu.run1(cpu.pc);         // DA
-      expect(cpu.pc).toBe(0x103);
-      //      expect(cpu.getCY()).toBe(+(digits[2] != 0));
-      expect(cpu.SFR[ACC]).toBe((digits[1]<<4) | digits[0]);
-    }
-  }
-});
+  cpu.run1(cpu.pc);         // DA
+  expect(cpu.pc).toBe(0x103);
+  expect(cpu.getCY()).toBe(+(bcdsum >>> 8 != 0));
+  expect(cpu.SFR[ACC]).toBe(bcdsum & 0xFF);
+}
 
 
+test(`ADDC/DA 0x00+0x00=0x00,CY=0`, () => testADDC_DA(0x00, 0x00));
+test(`ADDC/DA 0x02+0x02=0x04,CY=0`, () => testADDC_DA(0x02, 0x02));
+test(`ADDC/DA 0x77+0x41=0x18,CY=1`, () => testADDC_DA(0x77, 0x41));
+test(`ADDC/DA 0x37+0x41=0x78,CY=0`, () => testADDC_DA(0x37, 0x41));
+test(`ADDC/DA 0x57+0x73=0x30,CY=1`, () => testADDC_DA(0x57, 0x73));
+test(`ADDC/DA 0x77+0x47=0x24,CY=1`, () => testADDC_DA(0x77, 0x47));
