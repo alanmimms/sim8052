f1856ecf7bb65496f4e675253dc382e94b2df93f
Alan Mimms <alanmimms@gmail.com>
2019-02-11 08:59:34 -0800

    checkpoint

--- cpu-8052.js
+++ cpu-8052.js
@@ -4,14 +4,14 @@ const _ = require('lodash');
 
 
 /*
-# Access examples
+  # Access examples
 
-cpu.ACC = 0x12;
-cpu.ACC += 0x42;
-cpu.B = cpu.ACC;
+  cpu.ACC = 0x12;
+  cpu.ACC += 0x42;
+  cpu.B = cpu.ACC;
 
-cpu.PSW.OV = 1;
- */
+  cpu.PSW.OV = 1;
+*/
 
 
 const cpu = {};
@@ -20,21 +20,18 @@ module.exports.cpu = cpu;
 
 class SFR {
 
-  constructor(name, addr, resetValue) {
+  constructor(addr, resetValue) {
     this.addr = addr;
     this.resetValue = resetValue || 0x00;
-    this.name = name;
 
-    Object.defineProperty(cpu, name, {
+    Object.defineProperty(this, 'v', {
       enumerable: true,
 
       get: () => {
-        console.log(`get cpu.${name}=${this._v}`);
         return this._v;
       },
 
       set: v => {
-        console.log(`set cpu.${name}=${v}`);
         this._v = v;
       },
     });
@@ -44,8 +41,8 @@ class SFR {
 
 class BitAddressableSFR extends SFR {
 
-  constructor(name, addr, bitNames, resetValue) {
-    super(name, addr, resetValue);
+  constructor(addr, bitNames, resetValue) {
+    super(addr, resetValue);
     let parentObj = this;
 
     if (bitNames) {
@@ -88,32 +85,32 @@ class BitAddressableSFR extends SFR {
 
 
 const SFRs = {
-  PSW: new BitAddressableSFR('PSW', 0xD0, 'cy ac f0 rs1 rs0 ov ud p'),
-  ACC: new BitAddressableSFR('ACC', 0xE0),
-  B: new BitAddressableSFR('B', 0xF0),
-  SP: new SFR('SP', 0x81, 0x07),
-  DPL: new SFR('DPL', 0x82),
-  DPH: new SFR('DPH', 0x83),
-  P0: new BitAddressableSFR('P0', 0x80, null, 0xFF),
-  P1: new BitAddressableSFR('P1', 0x90, null, 0xFF),
-  P2: new BitAddressableSFR('P2', 0xA0, null, 0xFF),
-  P3: new BitAddressableSFR('P3', 0xB0, null, 0xFF),
-  IP: new BitAddressableSFR('IP', 0xB8, '. . pt2 ps pt1 px1 pt0 px0'),
-  IE: new BitAddressableSFR('IE', 0xA8, 'ea . et2 es et1 ex1 et0 ex0'),
-  TMOD: new SFR('TMOD', 0x89, 'gate1 ct1 t1m1 t1m0 gate0 ct0 t0m1 t0m0'),
-  TCON: new SFR('TCON', 0x88, 'tf1 tr1 tf0 tr0 ie1 it1 ie0 it0'),
-  T2CON: new SFR('T2CON', 0xC8, 'tf2 exf2 rclk tclk exen2 tr2 ct2 cprl2'),
-  TH0: new SFR('TH0', 0x8C),
-  TL0: new SFR('TL0', 0x8A),
-  TH1: new SFR('TH1', 0x8D),
-  TL1: new SFR('TL1', 0x8B),
-  TH2: new SFR('TH2', 0xCD),
-  TL2: new SFR('TL2', 0xCC),
-  RCAP2H: new SFR('RCAP2H', 0xCB),
-  RCAP2L: new SFR('RCAP2L', 0xCA),
-  SCON: new BitAddressableSFR('SCON', 0x98, 'sm0 sm1 sm2 ren tb8 rb8 ti ri'),
-  SBUF: new SFR('SBUF', 0x99),
-  PCON: new SFR('PCON', 0x87, 'smod . . . gf1 gf0 pd idl'),
+  PSW: new BitAddressableSFR(0xD0, 'cy ac f0 rs1 rs0 ov ud p'),
+  ACC: new BitAddressableSFR(0xE0),
+  B: new BitAddressableSFR(0xF0),
+  SP: new SFR(0x81, 0x07),
+  DPL: new SFR(0x82),
+  DPH: new SFR(0x83),
+  P0: new BitAddressableSFR(0x80, null, 0xFF),
+  P1: new BitAddressableSFR(0x90, null, 0xFF),
+  P2: new BitAddressableSFR(0xA0, null, 0xFF),
+  P3: new BitAddressableSFR(0xB0, null, 0xFF),
+  IP: new BitAddressableSFR(0xB8, '. . pt2 ps pt1 px1 pt0 px0'),
+  IE: new BitAddressableSFR(0xA8, 'ea . et2 es et1 ex1 et0 ex0'),
+  TMOD: new SFR(0x89, 'gate1 ct1 t1m1 t1m0 gate0 ct0 t0m1 t0m0'),
+  TCON: new SFR(0x88, 'tf1 tr1 tf0 tr0 ie1 it1 ie0 it0'),
+  T2CON: new SFR(0xC8, 'tf2 exf2 rclk tclk exen2 tr2 ct2 cprl2'),
+  TH0: new SFR(0x8C),
+  TL0: new SFR(0x8A),
+  TH1: new SFR(0x8D),
+  TL1: new SFR(0x8B),
+  TH2: new SFR(0xCD),
+  TL2: new SFR(0xCC),
+  RCAP2H: new SFR(0xCB),
+  RCAP2L: new SFR(0xCA),
+  SCON: new BitAddressableSFR(0x98, 'sm0 sm1 sm2 ren tb8 rb8 ti ri'),
+  SBUF: new SFR(0x99),
+  PCON: new SFR(0x87, 'smod . . . gf1 gf0 pd idl'),
 };
 module.exports.SFRs = SFRs;
 
@@ -135,18 +132,6 @@ const parityTable = [
 
 // Define the SFRs as module export symbols directly
 Object.keys(SFRs).forEach(sfr => module.exports[sfr] = SFRs[sfr]);
-
-
-if (require.main === module) {
-
-  // If this is run as main module, test a makeBits function a few
-  // ways.
-  const s1 = 'a7 b6 c5 d4 e3 f2 g1 h0';
-  console.log(`makeBits(0x80, "${s1}") =`, makeBits(0x80, s1));
-
-  const s2 = 'a7 b6 . d4 . f2 . h0';
-  console.log(`makeBits(0x90, "${s2}") =`, makeBits(0x90, s2));
-}
 
 
 if (require.main === module) {
